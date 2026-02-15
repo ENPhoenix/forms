9ecaa04b8c811fca7bec3443b27b47ad6b198631
ENPhoenix <nata94192@icloud.com>
2026-02-15 20:49:53 +0700

    Готово

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,6 @@
 import '../css/style.css';
 import Popover from './Popover';
 
-// Initialize all popovers when DOM is loaded
 document.addEventListener('DOMContentLoaded', () => {
   Popover.initAll();
 });
